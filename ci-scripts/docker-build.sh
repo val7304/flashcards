6c285f6ea6172c0b65939fcefddd7bd10042e331
val7304 <valerie_hermans@outlook.com>
2025-10-22 15:27:18 +0200

    Add Dockerfile

--- ci-scripts/docker-build.sh
+++ ci-scripts/docker-build.sh
@@ -3,3 +3,6 @@ set -e
 IMAGE_NAME="flashcards-app"
 echo "=== Building Docker image: $IMAGE_NAME ==="
 docker build -t $IMAGE_NAME .
+
+echo "Docker image '${IMAGE_NAME}' built successfully!"
+docker images | grep ${IMAGE_NAME}
