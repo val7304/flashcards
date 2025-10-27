5a058837f969a41a36d003145174a6376b8f56b3
val7304 <valerie_hermans@outlook.com>
2025-10-27 14:24:45 +0100

    push image to dockerhub

--- ci-scripts/docker-build.sh
+++ ci-scripts/docker-build.sh
@@ -5,4 +5,8 @@ IMAGE_NAME=${1:-flashcards}
 IMAGE_TAG=${2:-latest}
 
 echo "=== Building Docker image: ${IMAGE_NAME}:${IMAGE_TAG} ==="
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
+docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_TOKEN"
+docker build -t "$DOCKERHUB_USERNAME/flashcards:$IMAGE_TAG" .
+
+echo "=== Pushing image to DockerHub ==="
+docker push "$DOCKERHUB_USERNAME/flashcards:$IMAGE_TAG"
