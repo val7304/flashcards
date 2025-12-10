80e6a73ed1c3ab3a51bbaaf9f53493d5f82a5412
val7304 <valerie_hermans@outlook.com>
2025-12-10 21:09:49 +0100

    remove unused files

--- ci-scripts/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-IMAGE_NAME=${1:-flashcards}
-IMAGE_TAG=${2:-latest}
-
-echo "=== Building Docker image: ${IMAGE_NAME}:${IMAGE_TAG} ==="
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
--- ci-scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-echo "=== Running tests ==="
-./mvnw -B -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE:-prod} test
