761c5f0d088fa141db09e5ee8cd6d973abcabe8a
val7304 <valerie_hermans@outlook.com>
2025-12-29 19:13:31 +0100

    fix issue codeQL: Exposed Spring Boot actuators
    t cherry-pick --continue

--- src/test/java/com/example/flashcards/controller/CategoryControllerTest.java
+++ src/test/java/com/example/flashcards/controller/CategoryControllerTest.java
@@ -12,12 +12,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = CategoryController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class CategoryControllerTest {
 
   @Autowired private MockMvc mockMvc;
